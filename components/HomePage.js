c6641d2fc8d4d6c5fec7c46c53d164f940a5f065
Abish-x-lethal <magarabish456@gmail.com>
2023-05-14 16:54:50 +0545

    Final commit

--- components/HomePage.js
+++ components/HomePage.js
@@ -10,6 +10,8 @@ const HomePage = () => {
         </h2>
         <h1 className='frontend'>A Frontend dev here</h1>
      </div>
+     <button class="px-10 py-4 -mt-16 text-2xl bg-slate-700 text-white rounded-md shadow-sm hover:scale-125 ease-in-out duration-150 content-2">Hire Me</button>
+
      </>
   )
 }
